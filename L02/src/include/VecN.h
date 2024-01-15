822e5969db4656af21b0a8a558b75936dd9cf2d7
BrianMere <brianmere@cox.net>
2024-01-15 15:32:13 -0800

    Finished L02

---
+++ L02/src/include/VecN.h
@@ -0,0 +1,169 @@
+#ifndef VECN_H
+#define VECN_H
+
+#include <array>
+#include <iostream>
+#include <tuple>
+#include <cmath>
+#include <algorithm>
+
+/*
+    A static vector implementation class. Makes it so that we only deal with N-dimensional vectors
+*/
+template <typename T, int N>
+class VecN
+{
+private:
+public:
+
+    std::array<T,N> m_data;
+    // default constructor
+    VecN()
+    {
+        this->m_data = std::array<T,N>();
+        for (size_t i = 0; i < this->m_data.size(); i++)
+        {
+            this->m_data[i] = T(0);
+        }
+    };
+    // pass in an array of appropriate size and make a vector with that size...
+    VecN(std::vector<T> vec) {std::copy(std::begin(vec), std::end(vec), this->m_data.begin());}; 
+    VecN(std::array<T,N> & data) : m_data(data) {};
+    // default deconstructor
+    ~VecN() {};
+
+    // Operator Overrides...
+
+    T operator[](int i) const { return this->m_data.at(i); };
+    T& operator[](int i) { return this->m_data.at(i); };
+    VecN<T,N>& operator *(const VecN<T,N>& v) { return *this; };
+    // define scalar multiplication
+    VecN<T,N> operator *(double alpha) const
+    {
+        std::array<T,N> r_data = std::array<T,N>();
+        for (size_t i = 0; i < this->m_data.size(); i++)
+        {
+            r_data[i] = alpha * this->m_data.at(i);
+        }
+        return VecN<T,N>(r_data);
+    }
+    // define vector addition
+    VecN<T,N> operator +( VecN<T,N> & o_v) const
+    {
+        std::array<T,N> r_data = std::array<T,N>();
+        for (size_t i = 0; i < this->m_data.size(); i++)
+        {
+            r_data[i] = this->m_data.at(i) + o_v[i];
+        }
+        return VecN<T,N>(r_data);
+    }
+
+    // Now the following can be consructed from the two definitions above!
+
+    VecN<T,N> operator -( VecN<T,N> & o_v) const
+    {
+        VecN<T,N> in = o_v * -1.0;
+        return *this + in;
+    }
+
+    double length_squared() const
+    {
+        double sum = 0.0;
+        for (T elem : this->m_data)
+        {
+            sum += ((double)elem * (double)elem);
+        }
+        return sum;
+    }
+    double length() const { return sqrt(this->length_squared());}
+
+
+    VecN<T,N>& operator+=(const VecN<T,N> & o_v) 
+    {
+        for (size_t i = 0; i < N; i++)
+        {
+            (*this)[i] = (*this)[i] + o_v[i];
+        }
+        return *this;
+    }
+    VecN<T,N>& operator*=(double alpha) 
+    {
+        for (size_t i = 0; i < N; i++)
+        {
+            (*this)[i] *= alpha;
+        }
+        return *this;
+    }
+    VecN<T,N>& operator-=(const VecN<T,N> & o_v)
+    {
+        *this += o_v * (-1.0);
+        return *this;
+    }
+
+
+};
+
+template <typename T, int N>
+inline std::ostream& operator <<(std::ostream &out, const VecN<T,N> &v) 
+{
+    out << "[";
+    for (size_t i = 0; i < v.m_data.size(); i++)
+    {
+        T elem = v.m_data.at(i);
+        out << std::to_string(elem);
+        // only add "," if we aren't at the end
+        if(i != v.m_data.size() - 1)
+        {
+            out << ", ";
+        }
+    }
+    out << "]";
+    return out;
+    //return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+
+}
+
+typedef VecN<u_int8_t, 3> Color; // define Color as just a vector of 3 colors with 8 bits (max 0 to 255 is what we want)
+typedef VecN<double, 3> Vec3; // define a Vec3 (world coords) as our VecN with a double floating point values and 3 values
+typedef VecN<double, 4> Vec4; // same as Vec3 but with an extra coordinate (for math we do later on...)
+
+/**
+ * Ignore the top 8 bits of hex and create a Color object from the other values...
+*/
+inline Color color_from_hex(u_int32_t hex) 
+{
+    return Color({(hex & 0xFF0000) >> 16, (hex & 0x00FF00) >> 8, (hex & 0x0000FF) >> 0});
+}
+
+/**
+ * Calculate the determinant of two, 2-dimensional vectors (ie: in 2D, find the area that they encompass on their spanning parallelogram
+*/
+template <typename T>
+inline double calc_2determinant(VecN<T, 2> & v1, VecN<T, 2> & v2)
+{
+    return (v1[0] * v2[1]) - (v2[0] * v1[1]);
+}
+
+/**
+ * Calculate the Area of a triangle, given by 3 vertices...
+*/
+template <typename T>
+inline double calc_TriArea(VecN<T,2> & v1, VecN<T,2> & v2, VecN<T,2> & v3)
+{
+    VecN<T, 2> vT = v1 - v3;
+	VecN<T, 2> vP = v2 - v3;
+	return 0.5 * calc_2determinant(vT, vP);
+}
+
+
+// some default colors and vectors, for you to have fun with!!!
+Color RED = color_from_hex(0xFF0000);
+Color GREEN = color_from_hex(0x00FF00);
+Color BLUE = color_from_hex(0x0000FF);
+Color BLACK = color_from_hex(0x000000);
+Color YELLOW = color_from_hex(0xFFFF00);
+Color ORANGE = color_from_hex(0xFF8000);
+Color PURPLE = color_from_hex(0x7F00FF);
+Color WHITE = color_from_hex(0xFFFFFF);
+
+#endif
---
+++ L02/src/main.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <memory>
+#include "Image.h"
+
+#include "BoundingBox.h"
+#include "VecN.h"
+
+/**
+ * Turn a string into some generic passed type
+ * (taken from https://stackoverflow.com/questions/69238675/how-to-generically-convert-a-string-to-another-type-using-a-template-function)
+ *
+*/
+template <typename T>
+T getAs(const std::string& s)
+{
+	std::stringstream ss{s};
+	T t;
+	ss >> t;
+	return t;
+}
+
+/**
+ * Code to find a list of delimed substrings
+ * taken from https://sentry.io/answers/split-string-in-cpp/
+*/
+template <typename T>
+std::vector<T> getWords(std::string s, std::string delim)
+{
+	std::vector<T> res;
+	unsigned int pos = 0;
+	while(pos < s.size())
+	{
+		pos = s.find(delim);
+		res.push_back(getAs<T>(s.substr(0,pos)));
+		s.erase(0,pos+delim.size());
+		pos = 0;
+	}
+	return res;
+}
+
+/**
+ * Get nD Vertices a total of v_num number of times for some arbitrary type. 
+ * You must input a comma-separated list of the appropriate T type a total of N times to get the 
+*/
+template <typename T, int N>
+std::vector<VecN<T, N>> getVertex(unsigned int v_num)
+{
+
+	std::vector<VecN<T, N>> data;
+
+	std::cout << "Getting input for " << std::to_string(v_num) << " vertices..." << std::endl;
+	for(unsigned int i = 0; i < v_num; i++)
+	{
+		std::string in;
+		std::cout << "Input new Vertex (ex: \"T1, ..., (up to) T" << std::to_string(N) << "\" ): Currently on Vertex # " << std::to_string(i) << std::endl;
+		std::getline(std::cin, in);
+		// create a new vector from the list we made and push it to our data vector.
+		data.push_back(VecN<T,N>(getWords<T>(in, ",")));
+
+	}
+	return data;
+}
+
+int main(int argc, char **argv)
+{
+	/**
+	 * Read in 3 vertices from stdin to represent our triangle
+	 * Assign different color values to each vertex. 
+	 * Compute the bounding box of the triangle using L01
+	 * Computer the barycentric coordinates for every pixel in the bounding box
+	 * For each pixel within the triangle:
+	 *  - color the pixel using the interpolated color
+	 * Write out the pixel values as an image (from last time)
+	*/
+
+	// 0. Get the width and height as limiting factors...
+	unsigned int width = 500;
+	unsigned int height = 500;
+	auto image = std::make_shared<Image>(width, height);
+	const Color background = BLACK;
+
+	// 1. Get the data of each vertex 
+	auto vs = getVertex<int, 2>(3);
+	/* For debugging, print out the following information... */
+	std::cout << "You entered the following vertices: " << std::endl;
+	for(auto elem : vs)
+	{
+		std::cout << elem << std::endl;
+	}
+	/* Note that now that we have the vertices, we can calculate the area of the total triangle for later */
+	double A = calc_TriArea(vs[0], vs[1], vs[2]);
+
+
+	// 2. Assign different color values to each vertex (predefined)
+	std::vector<Color> cs;
+	std::cout << "Now asking for 3 colors for your vertices..." << std::endl;
+	std::cout << "Please enter in any combinations of your three colors. For example, enter \"r + g\" to get RED + GREEN" << std::endl;
+	std::cout << "You can enter in any colors from the following: (r: RED, g: GREEN, b: BLUE, y: YELLOW, o: ORANGE, p: PURPLE)" << std::endl;
+	for(unsigned int i = 0; i < 3; i++)
+	{
+		
+		Color temp;
+		std::string in;
+
+		std::cout << "Asking for Vertex " << std::to_string(i) << ":" << std::endl;
+		std::getline(std::cin, in);
+		std::vector<std::string> chosen_colors = getWords<std::string>(in, "+");
+		temp = BLACK;
+		for(std::string elem : chosen_colors)
+		{
+			if(elem.find("r") != elem.npos)
+				temp += RED;
+			if(elem.find("g") != elem.npos)
+				temp += GREEN;
+			if(elem.find("b") != elem.npos)
+				temp += BLUE;
+			if(elem.find("y") != elem.npos)
+				temp += YELLOW;
+			if(elem.find("o") != elem.npos)
+				temp += ORANGE;
+			if(elem.find("p") != elem.npos)
+				temp += PURPLE;
+		}
+		cs.push_back(temp);
+	}
+
+
+	// 3. Compute the bounding box of the triangle using L01 (remember that points are pixel coordinates)
+	BoundingBox<int, 2> bb = BoundingBox<int, 2>();
+	bb.add_vertex(vs[0]);
+	bb.add_vertex(vs[1]);
+	bb.add_vertex(vs[2]);
+	/* Now bb has the max and min points of our box, so just take combinations of points on the inside... */
+	for(int x = 0; x < width; x++)
+	{
+		for(int y = 0; y < height; y++)
+		{
+			// we iterate through all possible points here...
+			VecN<int, 2> test_pt = VecN<int, 2>({x, y});
+			if(bb.in_box(test_pt))
+			{
+				// 4. Compute the Barycentric coordinates for each point here
+				/* We already have A, we need to find the smaller areas for the subtriangles for each alpha, beta, gamma */
+				// for alpha, we exclude considering v0, for beta exclude v1, ... and substitute with test_pt
+				double alpha = calc_TriArea(test_pt, vs[1], vs[2]) / A;
+				double beta = calc_TriArea(test_pt, vs[2], vs[0]) / A;
+				double gamma = calc_TriArea(test_pt, vs[0], vs[1]) / A;
+			
+
+				// 5. Using the Barycentric Calculations prior, determine if we are in the triangle (ie: all values in (0,1) range)
+				// - if we are, color based on the alpha, beta, gamma values from (4), otherwise, color in as background...
+				if(	alpha >= 0 && alpha <= 1 &&
+					beta >= 0 && beta <= 1 &&
+					gamma >= 0 && gamma <= 1) // write our determined triangle color...
+				{
+					Color new_pixel_color = BLACK;
+					new_pixel_color += (cs[0] * alpha);
+					new_pixel_color += (cs[1] * beta);
+					new_pixel_color += (cs[2] * gamma);
+					image->setPixel(
+						x, 
+						y, 
+						new_pixel_color[0], 
+						new_pixel_color[1], 
+						new_pixel_color[2]
+					);
+				}
+				else // write background pixel
+				{
+					image->setPixel(
+						x,
+						y,
+						background[0],
+						background[1], 
+						background[2]
+					);
+				}
+			
+			}
+			else // write background pixel
+			{
+				image->setPixel(
+						x,
+						y,
+						background[0],
+						background[1], 
+						background[2]
+				);
+			}
+		}
+	}
+
+	// Very lastly, write to our output!!!
+	image->writeToFile("output.png");
+}
+
+
