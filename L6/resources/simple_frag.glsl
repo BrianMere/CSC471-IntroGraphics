3f49f404bed29f66b7dacfd92aaac291d5d0e086
BrianMere <brianmere@cox.net>
2024-02-14 14:00:26 -0800

    L6 almost done, need to fix specular lighting calcs in simple_frag.glsl

---
+++ L6/resources/simple_frag.glsl
@@ -0,0 +1,29 @@
+#version 330 core 
+
+out vec4 color;
+
+uniform vec3 MatAmb; // normal "color" of the object
+uniform vec3 MatDif; // diffuse "color"
+uniform vec3 MatSpec;
+uniform float MatShine;
+
+//interpolated normal and light vector in camera space
+in vec3 fragNor;
+in vec3 lightDir;
+//position of the vertex in camera space
+in vec3 EPos;
+
+void main()
+{
+	//you will need to work with these for lighting
+	vec3 normal = normalize(fragNor);
+	vec3 light = normalize(lightDir);
+
+	float dC = max(dot(normal, light), 0);
+	// float sC = max(dot(normal, ));
+
+	// note that color.a is our opacity, which we need to be 1!!!
+	color = dC * vec4(MatDif, 0.0); // diffuse light
+	color += MatShine * vec4(MatSpec, 0.0); // specular light
+	color += vec4(MatAmb, 1.0); // ambient light
+}
